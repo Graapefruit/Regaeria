381d4aa7cb114c6aa4d4bafd1bde346d618f57cf
Graham Buceta <bucetagr@gmail.com>
2021-04-22 00:47:00 -0400

    Initial Commit

---
+++ Assets/Pair.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pair<T, G> {
+    public T x;
+    public G z;
+
+    public Pair(T x, G z) {
+        this.x = x; 
+        this.z = z;
+    }
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+    public void setAsHighlighted() {
+        transform.GetComponent<SpriteRenderer>().color = new Color(0.9f, 0.9f, 0.9f, 1.0f);
+    }
+}
